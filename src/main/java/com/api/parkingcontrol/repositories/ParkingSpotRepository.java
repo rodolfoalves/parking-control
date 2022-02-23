e8130d4e1968adecdc292bb891db8240971f8e59
rodolfoalves <rodolfosalves.pro@gmail.com>
2022-02-23 01:17:06 -0300

    post, get, get_by_ID, delete and put methods implemented

--- src/main/java/com/api/parkingcontrol/repositories/ParkingSpotRepository.java
+++ src/main/java/com/api/parkingcontrol/repositories/ParkingSpotRepository.java
@@ -2,13 +2,24 @@ package com.api.parkingcontrol.repositories;
 
 import com.api.parkingcontrol.models.ParkingSpotModel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 // extends JpaRepository<"qual o model desse repository", "identificador">
 public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
 
+    //Anotações JPA para banco
+
+    public boolean existsByLicensePlateCar(String licensePlateCar);
+
+    public boolean existsByParkingSpotNumber(String parkingSpotNumber);
+
+    public boolean existsByApartmentAndBlock(String apartment, String block);
+
+    //public List<ParkingSpotModel> findAll();
 }
